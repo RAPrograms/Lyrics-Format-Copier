4aebebffeb8924e791e3f8a6bc1eae050454e5ba
RAPrograms <RAPrograms@outlook.com>
2026-01-28 22:49:44 +0000

    Correcting lyric parsing on genius

--- platforms/genius.js
+++ platforms/genius.js
@@ -1,25 +1,40 @@
 "use strict"
 
+/**
+ * @param {HTMLElement} container
+ */
+function recursiveRead(container, output, cache = ""){
+    for(const child of container.childNodes){
+        if(child.nodeName == "#text"){
+            cache += child.textContent
+            continue
+        }
+
+        if(child.getAttribute("data-exclude-from-selection") == "true")
+            continue
+
+        if(child.nodeName == "BR"){
+            output.push(cache)
+            cache = ""
+            continue
+        }
+
+        cache = recursiveRead(child, output, cache)
+    }
+
+    return cache
+}
+
+
 const data = new Promise(resolve => {
     const containors = document.querySelectorAll("#lyrics-root > [data-lyrics-container]")
     const lyrics = []
-    let cache = ""
 
     for(let i=0; i<containors.length; i++){
         const chunck = containors[i]
 
-        for(let j=1; j<chunck.childNodes.length; j++){
-            const node = chunck.childNodes[j]
-            if(node.nodeName == "BR"){
-                lyrics.push(cache)
-                cache = ""
-                continue
-            }
-
-            cache += node.textContent
-        }
+        lyrics.push(recursiveRead(chunck, lyrics))
     }
 
-    lyrics.push(cache)
     resolve(lyrics)
 })
